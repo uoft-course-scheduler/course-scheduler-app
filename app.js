1d115aafa6604bd2574920f2ae375ee510c7faaf
Zsw007 <zsw007@gmail.com>
2015-10-25 15:51:45 -0400

    Added the uoft route.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var uoft = require('./routes/uoft');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/uoft', uoft);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
