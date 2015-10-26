ff73c60ba64c5f7948d60a20787c8e92116355b7
Zsw007 <zsw007@gmail.com>
2015-10-25 20:12:57 -0400

    Began writing functionality to request the cobalt API.

---
+++ models/cobalt/course.js
@@ -0,0 +1,107 @@
+var http = require('http');
+var https = require('https');
+var querystring = require('querystring')
+
+/**
+ * The options for GETing a list of all courses.
+ * 
+ * @type {Object}
+ */
+var LIST_COURSES = {
+  host: 'cobalt.qas.im',
+  port: 443,
+  path: '/api/1.0/courses/list',
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+/**
+ * The options for GETing information for a specific course.
+ * Must replace the :id in path property with desired id.
+ * 
+ * @type {Object}
+ */
+var SHOW_COURSES = {
+  host: 'cobalt.qas.im',
+  port: 443,
+  path: '/api/1.0/courses/show/:id',
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+
+/**
+ * The options for GETing a list of course by a search criteria.
+ *
+ * @type {Object}
+ */
+var SEARCH_COURSES = {
+  host: 'cobalt.qas.im',
+  port: 443,
+  path: '/api/1.0/courses/search',
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+
+/**
+ * The Course Class.
+ * An instance of the course class contains information representing a single
+ * U of T course.
+ * 
+ * @param {String} key cobalt API key.
+ */
+var Course = function(key) {
+  this.key = key;
+};
+
+
+/**
+ * Returns an array of no more than num elements where each element is a Course
+ * object that represents a single U of T course. The array is created from
+ * a list of all registered U of T courses on Cobalt.
+ * 
+ * @static
+ * @param {Number} num optional. defaults to 10. The number of courses to return
+ *                               in the array, max 200.
+ * @return {Array} an array of Course objects.
+ */
+Course.list = function(num) {
+  num = num || 10;
+
+  // The 200 limit is imposed by the Cobalt API.
+  if (num > 200) {
+    num = 200;
+  }
+
+
+
+}
+
+
+
+function get(opt, q, callback) {
+  // Since options is supposed to be a constant, we don't want to mutate it.
+  var options = opt;
+
+  // If any key value pair can be passed in a RESTful way (as opposed to a 
+  // query string), then do that instead.
+  for (var key : q) {
+    var search = ':' + key;
+    if (options.path.indexOf(search) > -1) {
+      options.path.replace(search, q.key);
+      delete q.key;
+    }
+  }
+
+  var query = querystring.stringify(q);
+  options.path = options.path + '?' + query;
+
+  http.request(options, callback).end();
+}
--- routes/uoft.js
+++ routes/uoft.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
+var Course = require(path.join(__dirname, '../models/cobalt/course'));
+var Building = require(path.join(__dirname, '../models/cobalt/building'));
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
