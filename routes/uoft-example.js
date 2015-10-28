ceffb0f1c010dd5a5ece8de7d19a6c858948f233
Wayne Zhang <zsw007@gmail.com>
2015-10-27 23:07:23 -0400

    Moved Cobalt examples to seperate file and added a route to be used for the front end to call. fixes #18.

---
+++ routes/uoft-example.js
@@ -0,0 +1,72 @@
+/**
+ * EXAMPLE PAGE
+ *
+ * This is an example of how the cobalt API can be called.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+var path = require('path');
+var Cobalt = require(path.join(__dirname, '../models/cobalt/cobalt'));
+
+
+// List 20 courses, skip the first 10.
+router.get('/list', function(req, res, next) {
+  var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
+
+  cobalt.listCourses(function(a) {
+    for (var i = 0; i < a.length; i++) {
+      var str = JSON.stringify(a[i], null, 2);
+      res.write(str);
+    }
+
+    res.end();
+  }, 20, 10);
+
+});
+
+
+// Get the course with the id CSC148H1F20159.
+router.get('/get', function(req, res, next) {
+  var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
+
+  cobalt.getCourse('CSC148H1F20159', function(c) {
+    var str = JSON.stringify(c, null, 2);
+    res.write(str);
+
+    res.end();
+  });
+
+});
+
+// Get the course with the code CSC301H1F.
+router.get('/code', function(req, res, next) {
+  var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
+
+  cobalt.findCourse('CSC301H1F', function(c) {
+    var str = JSON.stringify(c, null, 2);
+    res.write(str);
+
+    res.end();
+  });
+
+});
+
+
+// Find courses with the word CSC, limit to 10 (default).
+router.get('/search', function(req, res, next) {
+  var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
+
+  cobalt.searchCourses('CSC', function(a) {
+    for (var i = 0; i < a.length; i++) {
+      var str = JSON.stringify(a[i], null, 2);
+      res.write(str);
+    }
+
+    res.end();
+  });
+
+});
+
+module.exports = router;
