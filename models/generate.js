28ad021e0b9702b94ed611a6b60ed5e326ab1a0c
Eugene Wong <i.am.youjean@gmail.com>
2015-10-31 17:51:48 -0400

    added generate permutations

---
+++ models/generate.js
@@ -0,0 +1,55 @@
+
+// need to fix this :()
+var permutations = []
+
+/**
+ * Generates a permutation of courses
+ * 
+ * @param a : json object containing the selected courses
+ */
+var Generate = function(a) {
+  return gen(a);
+}
+
+// calls helper to generate permutations
+function gen(a) {
+
+  var courseList = []
+
+  // each course is an entry so:
+  for (var i = 0; i < a.length; i++) {
+    courseList.push(a[i]);
+  }
+
+  // call helper to generate permutations
+  permutation(courseList, [], courseList.length);
+
+  return permutations;
+}
+
+
+// arrangement is the permutation of courses
+// starts empty, fills up on the way down
+// then gets pushed to the permutations list on deepest level
+function permutation(courses, arrangement) {
+  if (courses.length == 0) {
+    permutations.push(arrangement);
+  } else {
+
+    for (var meeting of courses[0].meeting_sections) {
+
+      console.log(courses[0].code);
+      var course = {
+        "meeting_section" : meeting,
+        "course_code" : courses[0].code
+      };
+      
+      var newArrangement = arrangement.slice(0);
+      newArrangement.push(course);
+
+      permutation(courses.slice(1), newArrangement);
+    }
+  }
+}
+
+module.exports = Generate;
