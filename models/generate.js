18a5866713694218eb94f25db0b64f8170aeac5b
Eugene Wong <i.am.youjean@gmail.com>
2015-10-31 19:54:39 -0400

    filtering out tutorials for course generation

--- models/generate.js
+++ models/generate.js
@@ -6,7 +6,7 @@ var permutations = [];
  * 
  * @param a : json object containing the selected courses
  */
-var Generate = function(a) {
+ var Generate = function(a) {
   return gen(a);
 }
 
@@ -34,18 +34,23 @@ function permutation(courses, arrangement) {
   if (courses.length == 0) {
     permutations.push(arrangement);
   } else {
+    for (var i = 0; i < courses[0].meeting_sections.length; i++) {
 
-    for (var meeting of courses[0].meeting_sections) {
+      // if its a lecture section, add the course.
+      if (courses[0].meeting_sections[i].code[0] == 'L') {
+        var course = {
+          "meeting_section" : courses[0].meeting_sections[i],
+          "course_code" : courses[0].code
+        };
+        var newArrangement = arrangement.slice(0);
+        newArrangement.push(course);
 
-      var course = {
-        "meeting_section" : meeting,
-        "course_code" : courses[0].code
-      };
-      
-      var newArrangement = arrangement.slice(0);
-      newArrangement.push(course);
+        permutation(courses.slice(1), newArrangement);
+      } else {
+        // tutorial, skip this iteration
+        continue; 
+      } 
 
-      permutation(courses.slice(1), newArrangement);
     }
   }
 }
