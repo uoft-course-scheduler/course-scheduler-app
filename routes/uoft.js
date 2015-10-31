b4c4998c11c0a4a468d148350c7019e64e4b5658
Zsw007 <zsw007@gmail.com>
2015-10-31 01:33:59 -0400

    Began working on the ability to get permutation of course arrangements.

--- routes/uoft.js
+++ routes/uoft.js
@@ -6,7 +6,6 @@ var path = require('path');
 var Cobalt = require(path.join(__dirname, '../models/cobalt/cobalt'));
 
 
-// List 20 courses, skip the first 10.
 router.get('/list', function(req, res, next) {
   var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
 
@@ -16,5 +15,40 @@ router.get('/list', function(req, res, next) {
   });
 
 });
+
+router.get('/generate', function(req, res, next) {
+  var cobalt = new Cobalt('Yu6lYuyoUmSjWVMShglIbQKbKPTZYwxk');
+
+  // We expect as GET parameter a list of course code seperated by comma.
+  var courses = req.query.courses.split(',');
+
+  if (courses.length < 1) {
+    res.end(JSON.stringify({}));
+  }
+
+  // We want to convert the course strings to a Cobalt Course object.
+  var cobaltCourses = [];
+
+  cobalt.findCourse(courses[0], function r(result) {
+
+    // Remove the first element from the array and add the result to the array
+    // of the cobalt Course objects. Note that we got the result from the
+    // callback function r. It is not related to the courses array anymore at
+    // this point.
+    courses.splice(0, 1);
+    cobaltCourses.push(result);
+
+    // Recursively get the rest of the courses.
+    if (courses.length > 0) {
+      cobalt.findCourse(courses[0], r);
+    } else {
+      res.end(JSON.stringify(cobaltCourses));
+    }
+
+  });
+
+  
+});
+
 
 module.exports = router;
