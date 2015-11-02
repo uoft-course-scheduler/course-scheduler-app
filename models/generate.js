2c6814bfd6adb1b2d7fa3c1a2d5c9774453338a0
Samir Patel <samirpatel@live.ca>
2015-11-02 02:28:19 -0500

    Fixed the empty meeting sections and incorrect course code problem

--- models/generate.js
+++ models/generate.js
@@ -34,29 +34,38 @@ function permutation(courses, arrangement) {
   if (courses.length == 0) {
     permutations.push(arrangement);
   } else {
-    for (var i = 0; i < courses[0].meeting_sections.length; i++) {
-      // if its a lecture section, add the course.
-      if (courses[0].meeting_sections[i].code[0] == 'L') {
+      // check to see that course is not empty i.e wrong course
+      if (courses[0].meeting_sections != null){
+        console.log("L1", courses);
+        //check to see that there are meeting sections for the course
+        if (courses[0].meeting_sections.length != 0){
+          for (var i = 0; i < courses[0].meeting_sections.length; i++) {
+            // if its a lecture section, add the course.
+            if (courses[0].meeting_sections[i].code[0] == 'L' && courses[0].meeting_sections[i].times.length != 0) {
+              var course = {
+                  "meeting_section" : courses[0].meeting_sections[i],
+                  "course_code" : courses[0].code
+              };
+              var newArrangement = arrangement.slice(0);
+              newArrangement.push(course);
 
-        // TODO: problem with empty meet sections.
-        if ((courses[0].meeting_sections.length == 0) || (courses[0].meeting_sections[i].times.length == 0)) {
-          continue;
-        }
-
-        var course = {
-          "meeting_section" : courses[0].meeting_sections[i],
-          "course_code" : courses[0].code
-        };
-        var newArrangement = arrangement.slice(0);
-        newArrangement.push(course);
-
-        permutation(courses.slice(1), newArrangement);
-      } else {
-        // tutorial, skip this iteration
-        continue; 
-      } 
+              permutation(courses.slice(1), newArrangement);
+            } else {
+            // tutorial, skip this iteration
+            continue; 
+            } 
+          }
+        } else {
+          //no meeting section, alert user, continue to next course
+          permutation(courses.slice(1), arrangement);
+          }
+      }
+      else{
+        //wrong course, alert user, continue to next course
+        permutation(courses.slice(1), arrangement);
+      }
     }
-  }
+    
 }
 
 module.exports = Generate;
